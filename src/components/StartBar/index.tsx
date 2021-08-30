1677758b17d7116efcb635f5698f36eb1a41f215
matias <matifandy@gmail.com>
2021-08-30 20:04:44 -0300

    add button start and clock

--- src/components/StartBar/index.tsx
+++ src/components/StartBar/index.tsx
@@ -1,10 +1,31 @@
 import "./index.css"
+import startbutton from './start.png';
+import sound from './soundicon.png';
+import second from './secondicon.png';
+import virus from './virusicon.png';
+
 
 const StartBar = () => {
+    const today = new Date()
+    const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
     return (
-        <div className="StartBar">
-            
+    <div className="StartBar">
+        <div className="items item-left">
+        <div className="button">
+            <img src={startbutton} alt="start"/>
+            </div>
         </div>
+        <div className="items item-right">
+        <img className="icon" src={sound} alt="start"/>
+        <img className="icon"  src={second} alt="start"/>
+        <img className="icon"  src={virus} alt="start"/>
+         
+            <div className="time">
+               {time}
+            </div>
+        </div>
+    </div>
     )
 }
 
